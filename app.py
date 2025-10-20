d13bf26ffbcfda3025ce3789e26959b48b3d85bd
Raafiya-Gouher <raafiya.rg@gmail.com>
2025-10-20 20:23:29 +0530

    Added Output

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 # ------------------------------
 # Simple Python web app using Flask.
-# Deployed automatically using GitHub Actions.
+# Deployed automatically using GitHub Actions on AWS EC2 instance.
 # ------------------------------
 
 from flask import Flask
@@ -11,6 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, World! from github actions"
+
 
 if __name__ == '__main__':
      app.run(host="0.0.0.0", port=5000)
